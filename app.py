3a9dec0cafbf1d153d672aeead008bcb327517d4
Rajeev Tripathi <141212857+Rajeev49@users.noreply.github.com>
2025-06-06 00:11:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('/Users/rajeevtripathi/Desktop/Multiple Disease Prediction System/Saved Models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('/Users/rajeevtripathi/Desktop/Multiple Disease Prediction System/Saved Models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('/Users/rajeevtripathi/Desktop/Multiple Disease Prediction System/Saved Models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 # sidebar for navigation
